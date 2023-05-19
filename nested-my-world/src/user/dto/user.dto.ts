2a02501c6baf69d27479e60630eced9136487dbc
zhou nightwish <95694686+myNightwish@users.noreply.github.com>
2023-05-20 00:27:59 +0800

    Delete nested-my-world directory

--- nested-my-world/src/user/dto/user.dto.ts
+++
@@ -1,6 +0,0 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
-import { UserEntity } from './user.entity';
-
-export class UserDto extends PartialType(
-  OmitType(UserEntity, ['id', 'createdAt', 'updatedAt'] as const),
-) {}
